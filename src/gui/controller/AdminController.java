c5a1b01a9c747ddcde1e09a9f5552dd79e7be052
Nnikolov2000 <nnikolov2000@gmail.com>
2022-05-04 18:44:50 +0200

    Pre-splitting Admin on tasks

--- src/gui/controller/AdminController.java
+++ src/gui/controller/AdminController.java
@@ -1,12 +1,37 @@
 package gui.controller;
 
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import gui.model.UserModel;
 import javafx.fxml.Initializable;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class AdminController implements Initializable {
+
+    @FXML
+    private Button adminBtn;
+
+    @FXML
+    private Button logoutBtn;
+
+    @FXML
+    private Button schoolBtn;
+
+    @FXML
+    private Button studentBtn;
+
+    @FXML
+    private Button teacherBtn;
 
     UserModel userModel;
 
@@ -17,5 +42,86 @@ public class AdminController implements Initializable {
     @Override
     public void initialize(URL location, ResourceBundle resources) {
 
+    }
+
+    public void onClickmanageTeachers(ActionEvent actionEvent) {
+        Stage currentStage = (Stage) teacherBtn.getScene().getWindow();
+        currentStage.close();
+        try{
+            Parent root = FXMLLoader.load(getClass().getResource("/gui/view/teacherManager.fxml"));
+            Stage stage = new Stage();
+            Scene scene = new Scene(root);
+            //stage.initStyle(StageStyle.TRANSPARENT);
+            stage.setScene(scene);
+            stage.show();
+            //scene.setFill(Color.TRANSPARENT);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
+    }
+
+    public void onClickmanageSchools(ActionEvent actionEvent) {
+        Stage currentStage = (Stage) schoolBtn.getScene().getWindow();
+        currentStage.close();
+        try{
+            Parent root = FXMLLoader.load(getClass().getResource("/gui/view/schoolManager.fxml"));
+            Stage stage = new Stage();
+            Scene scene = new Scene(root);
+            //stage.initStyle(StageStyle.TRANSPARENT);
+            stage.setScene(scene);
+            stage.show();
+            //scene.setFill(Color.TRANSPARENT);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    public void onClickmanageStudents(ActionEvent actionEvent) {
+        Stage currentStage = (Stage) studentBtn.getScene().getWindow();
+        currentStage.close();
+        try{
+            Parent root = FXMLLoader.load(getClass().getResource("/gui/view/studentManager.fxml"));
+            Stage stage = new Stage();
+            Scene scene = new Scene(root);
+            //stage.initStyle(StageStyle.TRANSPARENT);
+            stage.setScene(scene);
+            stage.show();
+            //scene.setFill(Color.TRANSPARENT);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    public void onClickmanageAdmins(ActionEvent actionEvent) {
+        Stage currentStage = (Stage) adminBtn.getScene().getWindow();
+        currentStage.close();
+        try{
+            Parent root = FXMLLoader.load(getClass().getResource("/gui/view/adminManager.fxml"));
+            Stage stage = new Stage();
+            Scene scene = new Scene(root);
+            //stage.initStyle(StageStyle.TRANSPARENT);
+            stage.setScene(scene);
+            stage.show();
+            //scene.setFill(Color.TRANSPARENT);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    public void onClickLogOutBtn(ActionEvent actionEvent) {
+        Stage currentStage = (Stage) logoutBtn.getScene().getWindow();
+        currentStage.close();
+        try{
+            Parent root = FXMLLoader.load(getClass().getResource("/gui/view/loginView.fxml"));
+            Stage stage = new Stage();
+            Scene scene = new Scene(root);
+           // stage.initStyle(StageStyle.TRANSPARENT);
+            stage.setScene(scene);
+            stage.show();
+           // scene.setFill(Color.TRANSPARENT);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
     }
 }
